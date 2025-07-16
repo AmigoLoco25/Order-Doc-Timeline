cfbfa4d7a04330a22c43924619a6ca6cf1f9dcf5
AmigoLoco25 <aifs@wio.eco>
2025-07-16 12:31:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ def build_table():
     for col in date_cols:
         df[col] = df[col].dt.strftime("%d-%m-%Y")
         
-    df["__sort_date"] = pd.to_datetime(df["Presupuesto Date"], format="%d-%m-%Y", errors="coerce")
+    df["__sort_date"] = pd.to_datetime(df["Pedido Date"], format="%d-%m-%Y", errors="coerce")
     df = df.sort_values("__sort_date", ascending=False).drop(columns="__sort_date")
     
     # Reorder final columns
